8b450363e4e00179b06aca0add4cae9a8cafe729
Chris-wh233 <128221293+Chris-wh233@users.noreply.github.com>
2026-02-24 15:17:05 +0800

    enable webpack for dify-web >=1.12.0

--- langgenius/dify/resources/update.sh
+++ langgenius/dify/resources/update.sh
@@ -9,6 +9,8 @@ if [ $# -ne 1 ]; then
 fi
 
 version="$1"
+major_ver=$(echo "$version" | cut -d. -f1)
+minor_ver=$(echo "$version" | cut -d. -f2)
 target_dir="$version"
 
 # 创建目标目录
@@ -18,9 +20,11 @@ mkdir -p "$target_dir"
 tar -xzf $version-src.tar.gz -C $target_dir --strip-components=1
 
 sed -i '/^FROM node/c\FROM node:22-alpine-3.22 AS base' "$target_dir/web/Dockerfile" # web
-sed -i "/RUN pnpm build:docker/i \
+if [ "$major_ver" -gt 1 ] || ([ "$major_ver" -eq 1 ] && [ "$minor_ver" -ge 12 ]); then
+    sed -i "/RUN pnpm build:docker/i \
 RUN sed -i 's/next build/next build --webpack/' package.json \\
 ENV NEXT_FORCE_WEBPACK=1" "$target_dir/web/Dockerfile"
+fi
 
 cp api-dockerfile.template "$target_dir/api/Dockerfile" # api
 
