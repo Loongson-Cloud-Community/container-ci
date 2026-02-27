8063495e40ba222422f6b87f0ac3a184d1ebea82
znley <shanjiantao@loongson.cn>
2026-02-27 14:42:34 +0800

    library/erlang: refactoring process_version.sh

--- library/erlang/process_version.sh
+++ library/erlang/process_version.sh
@@ -1,74 +1,117 @@
 #!/bin/bash
 
-set -eo pipefail
+set -euo pipefail
+set -x
 
 source "$(dirname $0)/lib.sh"
 
 readonly REGISTRY='lcr.loongnix.cn'
 readonly ORG='library'
 readonly PROJ='erlang'
+readonly SRCS='sources'
+readonly IMAGE="$REGISTRY/$ORG/$PROJ"
+
+# 29.0.1-rc1
+version="$1"
+
+major_version="${version//.*/}"
+
+declare -ar ALPINE_TAGS=(
+    "alpine"
+    "$version-alpine"
+    "$major_version-alpine"
+)
+
+declare -ar DEBIAN_TAGS=(
+    "latest"
+    "$version"
+    "$major_version"
+)
+
+declare -ar SLIM_TAGS=(
+    "slim"
+    "$version-slim"
+    "$major_version-slim"
+)
+
+declare -Ar VARIANTS=(
+    ['alpine']="${ALPINE_TAGS[@]}"
+    ['debian']="${DEBIAN_TAGS[@]}"
+    ['slim']="${SLIM_TAGS[@]}"
+)
 
 # Prepare $version
 prepare()
 {
 
-    local version="$1"
     log INFO "Preparing version $version"
+    
+    mkdir -pv $SRCS
+    rm -rf $SRCS/docker-erlang-opt
+    git clone --depth=1 https://github.com/erlang/docker-erlang-otp $SRCS/docker-erlang-opt
 
-
-    pushd template > /dev/null || {
-        log ERROR "Failed to enter template directory: $SOURCES_DIR"
-        exit 1
-    }
-
-    ./versions.sh "$version" || {
-        log ERROR "${template_dir}/versions.py script failed for version: $version"
-    }
-
-    ./apply-templates.sh || {
-        log ERROR "${template_dir}/apply-templates.sh script failed for version: $version"
-        exit 1
-    }
-
+    pushd $SRCS/docker-erlang-opt/$major_version
+    for dockerfile in $(find * -name Dockerfile); do
+        sed -i '/FROM/s/trixie/forky/g' $dockerfile
+    done
     popd
 }
 
-docker_build(){
-    local version="$1"
-    local image="${REGISTRY}/${ORG}/${PROJ}:${version}"
-    log INFO "Building Docker image: $image"
+docker_build() {
+    local dockerfile="$1"
+    local targets="$2"
+    local context="$3"
 
-	# 获取到所有的 dockerfile
-	local dockerfiles=$(find ./template/$version -name 'Dockerfile')
+    local cmd="docker build"
+    cmd+=" -f $dockerfile"
 
-	for dockerfile in $dockerfiles; do
-		local build_dir=$(dirname $dockerfile)
-		make image -C $build_dir
-	done
+    local target_str=""
+    for target in ${targets[@]}; do
+        cmd+=" -t $target"
+    done
+    cmd+=" $context"
 
-	log INFO "Successfully built image: $image"
-
+    log INFO "$cmd"
+    $cmd
 }
 
-docker_push(){
-    local version="$1"
-
-	# 获取到所有的 dockerfile
-	local dockerfiles=$(find ./template/$version -name 'Dockerfile')
-
-	for dockerfile in $dockerfiles; do
-		local build_dir=$(dirname $dockerfile)
-		make push -C $build_dir
-	done
-}
-
-process()
+# build_variant alpine-slim context
+build_variant()
 {
-    version=$1
-    prepare $version
-    docker_build $version
-    docker_push $version
+    local variant="$1"
+    local context="$2"
+    local targets=()
+    local tags=${VARIANTS["$variant"]}
+    for tag in ${tags[@]}; do
+    # 同时构建 lcr.loongnix./x/y:tag 和 y:tag 以解决存在镜像依赖的情况
+        targets+=("$IMAGE:$tag" "$PROJ:$tag")
+    done
+    docker_build "$context/Dockerfile" "${targets[*]}" "$context"
 }
 
-process $1
+build()
+{
+    CONTEXT_PREFIX=$SRCS/docker-erlang-opt
+    build_variant "debian" "$CONTEXT_PREFIX/$major_version"
+    build_variant "alpine" "$CONTEXT_PREFIX/$major_version/alpine"
+    build_variant "slim" "$CONTEXT_PREFIX/$major_version/slim"
+}
+
+upload(){
+    for variant in ${!VARIANTS[@]}; do
+        local tags="${VARIANTS[$variant]}"
+        for tag in ${tags[@]}; do
+            docker push $IMAGE:$tag
+        done
+    done
+}
+
+main()
+{
+    prepare
+    build
+    upload
+}
+
+main
 
