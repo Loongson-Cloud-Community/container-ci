289114845deb8aadbfaab62c02a75b770599f6d1
znley <shanjiantao@loongson.cn>
2026-04-29 09:44:54 +0800

    alpine: update version parameter format from x.y to x.y.z

--- library/alpine/fetch_versions.sh
+++ library/alpine/fetch_versions.sh
@@ -5,16 +5,16 @@ readonly FTP_BASE='https://cz.alpinelinux.org/alpine'
 
 fetch_versions(){
 
-    local versions=$(wget ${FTP_BASE} -qO- \
-		| grep -o '>v.*<' \
-		| grep -v '\-rc' \
-		| sed -r 's:>v(.*)\/<:\1:' )
+     local versions=$(wget -qO- https://cz.alpinelinux.org/alpine/latest-stable/releases/loongarch64 \
+        | grep -oP 'minirootfs-\K[\d\.]+(?=-loongarch64\.tar\.gz)' \
+        | sort -V \
+        | uniq)
+
 
     echo "$versions" \
         | sort -V \
         | grep -Fxv -f processed_versions.txt \
         | { grep -Fxv -f ignore_versions.txt || [ $? -eq 1 ]; }
-
 }
 
 
