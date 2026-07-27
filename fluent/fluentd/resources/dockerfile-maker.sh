ba439feaf390c29d55cad38283be4a69d3abe0d2
Chris-wh233 <128221293+Chris-wh233@users.noreply.github.com>
2026-07-27 16:08:56 +0800

    fluentd: correct version in template

--- fluent/fluentd/resources/dockerfile-maker.sh
+++ fluent/fluentd/resources/dockerfile-maker.sh
@@ -12,6 +12,8 @@ RUBY_MAJOR_VER=$(sed -n 's/FROM ruby:\([0-9]\+\)\..*/\1/p' "$CONTEXT/Dockerfile"
 sed -i "s#FROM ruby:.*#FROM lcr.loongnix.cn/library/ruby:$RUBY_MAJOR_VER-slim#" "$CONTEXT/Dockerfile"
 sed -i '/buildDeps=/a\
       libssl-dev pkg-config \\' "$CONTEXT/Dockerfile"
+TEMP_VER=$(sed -n 's/.*Version="\([0-9]\+\.[0-9]\+\.[0-9]\+\)".*/\1/p' "$CONTEXT/Dockerfile" | head -n 1)
+sed -i "s/$TEMP_VER/$VERSION/" "$CONTEXT/Dockerfile"
 
 if [ "$MAJOR_VER" -eq 1 ] && [ "$MINOR_VER" -le 16 ]; then
     sed -i 's/ca-certificates/& tini/' "$CONTEXT/Dockerfile"
